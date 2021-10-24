214ca61310c1fc12cbebfac83703b085c4661eef
Vladimir <privezenoff@yandex.ru>
2021-10-25 00:11:35 +0300

    test: without moduleLogger.ts

--- plugins/moduleLogger.ts
+++ plugins/moduleLogger.ts
@@ -22,31 +22,31 @@ class ModuleLogger {
             (normalModuleFactory) => {
                 normalModuleFactory.hooks.module.tap('ModuleLogger', (_module, _createData) => {
                     // @ts-ignore
-                    this.fsNames.delete(`${_createData.resource}`);
+                    //this.fsNames.delete(`${_createData.resource}`);
                     return _module;
                 });
             }
         );
-        compiler.hooks.compile.tap('ModuleLogger', () => {
-            this.getPathsAllFiles(this.path);
-        });
-        compiler.hooks.afterDone.tap('ModuleLogger', () => {
-            fs.writeFileSync(this.pathToSave, JSON.stringify(Array.from(this.fsNames)));
-        });
+        // compiler.hooks.compile.tap('ModuleLogger', () => {
+        //     this.getPathsAllFiles(this.path);
+        // });
+        // compiler.hooks.afterDone.tap('ModuleLogger', () => {
+        //     fs.writeFileSync(this.pathToSave, JSON.stringify(Array.from(this.fsNames)));
+        // });
     }
 
-    getPathsAllFiles(path: string) {
-        const files = fs.readdirSync(path);
-        for (const file of files) {
-            const pathOfFile = `${path}\\${file}`;
-            const isDirectory = fs.lstatSync(pathOfFile).isDirectory();
-            if (isDirectory) {
-                this.getPathsAllFiles(pathOfFile);
-            } else {
-                this.fsNames.add(pathOfFile);
-            }
-        }
-    }
+    // getPathsAllFiles(path: string) {
+    //     const files = fs.readdirSync(path);
+    //     for (const file of files) {
+    //         const pathOfFile = `${path}\\${file}`;
+    //         const isDirectory = fs.lstatSync(pathOfFile).isDirectory();
+    //         if (isDirectory) {
+    //             this.getPathsAllFiles(pathOfFile);
+    //         } else {
+    //             this.fsNames.add(pathOfFile);
+    //         }
+    //     }
+    // }
 }
 
 export default ModuleLogger;
